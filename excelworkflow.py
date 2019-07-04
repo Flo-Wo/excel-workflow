9c422c5439a684b9eb5dc372e15ef75ed9e53b11
Flo-Wo <florianwolf1710@t-online.de>
2019-07-04 15:16:36 +0200

    small bug fixes

--- excelworkflow.py
+++ excelworkflow.py
@@ -6,21 +6,29 @@ import excelworkflowutilities as ewf
 import xlwings as xw
 
 #wb = xw.Book()
-wb = xw.Book('Ergebnisse.xlsx')
-sht = wb.sheets['Tabelle1']
-#print(sht.range('K13:K16').value)
-#print(type(sht.range('K13').value))
+path = str(input("Pfad zur Datei eingeben (mit .xlsx): "))
+sheet = str(input("Tabelle eingeben (z.B. 'Tabelle1'): "))
+valuesInput = str(input("Eingabespalten eingeben (z.B. 'M10:M38'): "))
+uncertaintiesInput = str(input("Eingabespalten eingeben (z.B. 'N10:N38'): "))
+resultsInput = str(input("Ziel eingeben für Gauss-Notation (z.B 'X10:X38'): "))
 
-valuesArray = sht.range('M10:M38').value
-uncertaintiesArray = sht.range('N10:N38').value
-#resultsArray = sht.range('X10:X38').value
+
+wb = xw.Book(path)
+sht = wb.sheets[sheet]
+
+
+valuesArray = sht.range(valuesInput).value
+uncertaintiesArray = sht.range(uncertaintiesInput).value
+resultsArray = sht.range(resultsInput).value
 
 #Anweisungen, dass Unsicherheiten der zweite Array sein müssen
 
 
-j = 10
-sht.range('X9').value = "Test Überschrift"
-if ((len(valuesArray) == len(uncertaintiesArray) and len(resultsArray) == len(valuesArray)):
+j = int(ewf.getStartIndex(resultsInput))
+print(j)
+#j = 10
+sht.range('X9').value = "shesh"
+if ( (len(valuesArray) == len(uncertaintiesArray) and len(resultsArray) == len(valuesArray))):
     for x in range(0, len(valuesArray)):
         # Check if there is an entry
         if valuesArray[x] != None:
@@ -31,11 +39,11 @@ if ((len(valuesArray) == len(uncertaintiesArray) and len(resultsArray) == len(va
             uncertaintyRounded = np.round(numbers[1][0], significantdigit)
             valueRounded = np.round(numbers[0][0], significantdigit)
             #print(str(ewf.gaussNotation(valueRounded, uncertaintyRounded)) + 'E' + str(int(numbers[1][1])) )
-            sht.range('X' + str(j)).value = str(ewf.gaussNotation(valueRounded, uncertaintyRounded)) + 'E' + str(int(numbers[1][1]))
+            sht.range(str(resultsInput[0]) + str(j)).value = str(ewf.gaussNotation(valueRounded, uncertaintyRounded)) + 'E' + str(int(numbers[1][1]))
         j += 1
 elif (len(valuesArray) != len(uncertaintiesArray)):
     print("Fehler: Länge der Eingaben stimmt nicht überein.")
-else :
+else:
     print("Fehler: Nicht genügend Platz für die Ergebnisse. Ein- und Ausgabegrößen müssen übereinestimmen.")
 
 
